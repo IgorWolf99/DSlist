b9c70ede43ee30284929d41c6c3ca233f8e73e09
Igor Wolf <igor.wolf99@gmail.com>
2023-05-20 01:39:04 -0300

    GET Game by ID

--- src/main/java/com/igorwolf/dslist/services/GameService.java
+++ src/main/java/com/igorwolf/dslist/services/GameService.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.igorwolf.dslist.dto.GameDTO;
 import com.igorwolf.dslist.dto.GameMinDTO;
-import com.igorwolf.dslist.entities.Game;
 import com.igorwolf.dslist.repositories.GameRepository;
 
 @Service
@@ -15,11 +16,18 @@ public class GameService {
 	@Autowired
 	public GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		var result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id){
+		var result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
 	
 }
